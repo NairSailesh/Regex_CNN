4b1a20cd229ac248fd689f0f22ed70a53f1c1dd5
Sailesh Nair <nairsailesh25@gmail.com>
2021-08-08 20:25:26 +0530

    updated
    image view on upload working
    todo: work on accuracy of predictions

--- PythonDocker/web_apps/static/main.js
+++ PythonDocker/web_apps/static/main.js
@@ -1,24 +1,4 @@
-$(document).ready(function () {
-    // Init
-    $('.image-section').hide();
-    $('#btn-predict').hide();
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#btn-predict').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#myfile").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-         readURL(this);
-    });    
-
-});
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
